bfe61cadc01ab7d8532349e47d81997ca53116c2
mineshmshah <mineshmshah@hotmail.co.uk>
2017-10-31 12:46:45 +0000

    finished ws-steps file with new updated workshop steps #4

--- solution/src/app.js
+++ solution/src/app.js
@@ -20,7 +20,7 @@ app.engine(
     layoutsDir: path.join(__dirname, 'views', 'layouts'),
     partialsDir: path.join(__dirname, 'views', 'partials'),
     defaultLayout: 'main',
-    helpers: helpers,
+    helpers,
   })
 );
 
